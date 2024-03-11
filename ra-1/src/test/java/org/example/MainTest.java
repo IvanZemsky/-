95bf620479206b2da841cbe034b696634007af49
IvanZemsky <i.ivanzemsky2005@gmail.com>
2024-03-11 19:46:11 +0300

    Rest Assured Java

---
+++ ra-1/src/test/java/org/example/MainTest.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @Test
+    void get_users() {
+        Main.get_users();
+    }
+}
